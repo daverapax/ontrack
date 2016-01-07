205ed8af2a4c08bca92532bdbd555215b54e1640
daverapax <generation.doof@googlemail.com>
2016-01-07 13:33:32 +0100

    Scaffold

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,50 @@
 class ProjectsController < ApplicationController
 	def index
+		@projects = Project.all
+	end
+
+	def edit
+		@project = Project.find(params[:id])
+	end
+
+	def new
+		@project = Project.new
+	end
+
+	def create
+		@project = Project.new
+		if @project.update(project_params)
+			flash[:notice] = "speichern erfolgreich"
+			redirect_to project_path(@project.id)
+		else
+			render "new"
+		end
+	end
+
+	def update
+		@project = Project.find(params[:id])
+		if @project.update(project_params)
+			flash[:notice] = "speichern erfolgreich"
+			redirect_to project_path(@project.id)
+		else
+			render "edit"
+		end
+	end
+
+	def show
+		@project = Project.find(params[:id])
+	end
+
+	def destroy
+
+		@project = Project.find(params[:id])
+		@project.destroy
+		redirect_to projects_url
 
 	end
+
+	private
+		def project_params
+			params.require(:project).permit(:title, :description, :start_date)
+		end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160106115500) do
+ActiveRecord::Schema.define(version: 20160107101613) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -20,5 +20,17 @@ ActiveRecord::Schema.define(version: 20160106115500) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.integer  "project_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "tasks", ["project_id"], name: "index_tasks_on_project_id"
 
 end
